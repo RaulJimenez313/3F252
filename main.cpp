a073e8e38cb942eff647bf5f9a284adfd7112011
RaulJimenez313 <a24310313@live.ceti.mx>
2025-09-03 02:40:30 +0000

    Mi prmer commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<Foco.hpp>
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hola mundo"<<endl;
+    Foco foco;
+    foco.Encender();
+    cout<<"EstadoFoco:"
+        <<foco.LeerEstado()
+        << endl;
+    return 0;
+}
+
